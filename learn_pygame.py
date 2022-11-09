3e6c4f1005a7ff27b68a6a49ed6a6f89fb1684ed
JohnnyLue <90609939+JohnnyLue@users.noreply.github.com>
2022-11-10 04:09:20 +0800

    Update learn_pygame.py

--- learn_pygame.py
+++ learn_pygame.py
@@ -14,7 +14,7 @@ BLACK=(0,0,0)
 SPEED = 15 
 HEADSIZE=25
 BODYSIZE=18
-FRUITSIZE=10
+FRUITSIZE=15
 
 head_img =pygame.image.load(os.path.join('img','circle.png'))
 body_img =pygame.image.load(os.path.join('img','circle.png'))
